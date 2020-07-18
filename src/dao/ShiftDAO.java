18104e4bd52a408c6b4490237a019f93c68b9fab
Jimmy <thinhnpptit@gmail.com>
2020-07-18 15:31:19 +0700

    add doctor form

---
+++ src/dao/ShiftDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import static dao.DAO.con;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Client;
+import model.Shift;
+
+/**
+ *
+ * @author thinh
+ */
+public class ShiftDAO extends DAO {
+
+    public Shift getShiftById(int id) {
+        Shift shift = null;
+        String sql = "SELECT * FROM tbshift WHERE id=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                shift.setId(rs.getInt("id"));
+                shift.setHours(rs.getString("hour"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return shift;
+    }
+}
